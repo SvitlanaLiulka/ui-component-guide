180a80e42362960bdf726505817e5905e094ec76
Matsvei Sakharuk <matsvei.sakharuk@paysera.com>
2023-02-13 12:02:33 +0100

    SUPPORT-68852: update HelpBlock story

--- src/HelpBlock/helpBlock.stories.jsx
+++ src/HelpBlock/helpBlock.stories.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
-import {
-    Form, FormGroup, HelpBlock as HelpBlockComponent,
-} from '@paysera/react-common';
+import { HelpBlock as HelpBlockComponent, Form, FormGroup } from '@paysera/react-common';
+import { getComponentPropType } from '../utils/getComponentPropType';
 import withReduxForm from '../utils/withReduxForm';
 
+HelpBlockComponent.displayName = 'HelpBlock';
+
 export default {
-    title: 'HelpBlock',
-    component: Form,
+    title: 'Help Block/HelpBlock',
+    component: HelpBlockComponent,
     decorators: [
         withReduxForm,
     ],
+    args: {
+        children: 'HelpBlock',
+    },
+    argTypes: {
+        children: {
+            control: 'text',
+            ...getComponentPropType('string'),
+        },
+        className: {
+            control: 'text',
+            ...getComponentPropType('string'),
+        },
+    },
 };
 
 const Template = args => (
-    <Form {...args}>
-        <FormGroup placeholder="Test" />
-        <HelpBlockComponent>I`m helper block</HelpBlockComponent>
+    <Form>
+        <FormGroup placeholder='Test' />
+        <HelpBlockComponent {...args} />
     </Form>
 );
 
